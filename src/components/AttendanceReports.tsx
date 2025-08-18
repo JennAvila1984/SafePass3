13d87a6f2711eba1f7a223ada084a115378d2b3f
JennAvila1984 <jennifer-avila1984@hotmail.com>
2025-08-18 13:09:08 -0400

    Update AttendanceReports.tsx

--- src/components/AttendanceReports.tsx
+++ src/components/AttendanceReports.tsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+const AttendanceReports: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Attendance Reports</h1>
+      <p>This is the Attendance Reports page. Reports will be shown here.</p>
+    </div>
+  );
+};
+
+export default AttendanceReports;
 
